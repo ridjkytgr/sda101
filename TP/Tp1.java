e0088d53f96f46038c06380fa53160264677f0e7
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-09-22 14:45:55 +0800

    Implement Agent class to store informations

--- TP/Tp1.java
+++ TP/Tp1.java
@@ -3,3 +3,60 @@ public class Tp1 {
 
     }
 }
+
+class Agent {
+    private char specialization;
+    private String code;
+    private int ascend;
+    private int descend;
+    private int currentRank;
+    private boolean isNeverIncrease = true;
+
+    public Agent(String code, char specialization) {
+        this.code = code;
+        this.specialization = specialization;
+        this.ascend = 0;
+        this.descend = 0;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public char getSpecialization() {
+        return this.specialization;
+    }
+
+    public int getAscend() {
+        return this.ascend;
+    }
+
+    public int getDescend() {
+        return this.descend;
+    }
+
+    public int getCurrentRank() {
+        return this.currentRank;
+    }
+
+    public boolean getIsNeverIncrease() {
+        return this.isNeverIncrease;
+    }
+
+    /**
+     * Menghitung dan me-return banyak rank 1 dan turun rank terakhir suatu agen.
+     * 
+     * @return integer banyak rank 1 dan turun rank terakhir suatu agen.
+     */
+    public int countAscDes() {
+        return this.descend + this.ascend;
+    }
+
+    /**
+     * Mengubah status dari isNeverIncrease menjadi false jika suatu agen pernah
+     * naik rank.
+     */
+    public void changeStatus() {
+        this.isNeverIncrease = false;
+    }
+}
