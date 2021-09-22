663e455fc72bbf909d578d74334be407f3b4d584
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-09-22 16:02:19 +0800

    Implement input system

--- TP/Tp1.java
+++ TP/Tp1.java
@@ -1,5 +1,112 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.*;
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+
 public class Tp1 {
-    public static void main(String[] args) {
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static void main(String args[]) throws IOException {
+
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+
+        int batch;
+        int agents;
+        int days;
+        int events;
+
+        batch = in.nextInt();
+
+        for (int tmp = 0; tmp < batch; tmp++) {
+            // Prompt for agents data
+            agents = in.nextInt();
+            for (int agent = 0; agent < agents; agent++) {
+                String prompt = in.next();
+                String[] arrOfPrompt = prompt.split(" ");
+
+                String agentCode = arrOfPrompt[0];
+                char agentSpecialization = arrOfPrompt[1].charAt(0);
+            }
+
+            // Prompt for ammount of days
+            days = in.nextInt();
+            for (int day = 0; day < days; day++) {
+                // Prompt for ammount of siesta appointing someone events
+                events = in.nextInt();
+                for (int event = 0; event < events; event++) {
+                    String prompt = in.next();
+                    String[] arrOfPrompt = prompt.split(" ");
+
+                    String agentCode = arrOfPrompt[0];
+                    int eventCode = Integer.parseInt(arrOfPrompt[1]);
+                }
+            }
+
+            // Prompt for last evaluation
+            String prompt = in.next();
+            String[] arrOfPrompt = prompt.split(" ");
+            String evalCommand = arrOfPrompt[0];
+
+            // Jika panutan & deploy
+            if (!arrOfPrompt[1].isEmpty()) {
+                int num = Integer.parseInt(arrOfPrompt[1]);
+
+                if (evalCommand.equals("PANUTAN")) {
+                    out.println(panutan(num));
+                } else {
+                    out.println(deploy(num));
+                }
+            } else {
+                if (evalCommand.equals("KOMPETITIF")) {
+                    out.println(kompetitif());
+                } else if (evalCommand.equals("EVALUASI")) {
+                    out.println(evaluasi());
+                } else {
+                    out.println(duo());
+                }
+            }
+        }
+
+        out.flush();
+    }
+
+    // taken from https://codeforces.com/submissions/Petr
+    // together with PrintWriter, these input-output (IO) is much faster than the
+    // usual Scanner(System.in) and System.out
+    // please use these classes to avoid your fast algorithm gets Time Limit
+    // Exceeded caused by slow input-output (IO)
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
 
     }
 }
