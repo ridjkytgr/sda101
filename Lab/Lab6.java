eece9a936f7925aded16fb55e796be9eefce55bc
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-11-15 15:17:06 +0800

    Init template (revise)

---
+++ Lab/Lab6.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+class Lab6 {
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+
+        List<Integer> tanah = new ArrayList<>();
+
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int height = in.nextInt();
+            tanah.add(height);
+        }
+
+        int Q = in.nextInt();
+        while (Q-- > 0) {
+            String query = in.next();
+            if (query.equals("A")) {
+                // TODO: Handle query A
+            } else if (query.equals("U")) {
+                // TODO: Handle query U
+            } else {
+                // TODO: Handle query R
+            }
+        }
+
+        out.flush();
+    }
+
+    // taken from https://codeforces.com/submissions/Petr
+    // together with PrintWriter, these input-output (IO) is much faster than the
+    // usual Scanner(System.in) and System.out
+    // please use these classes to avoid your fast algorithm gets Time Limit
+    // Exceeded caused by slow input-output (IO)
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
