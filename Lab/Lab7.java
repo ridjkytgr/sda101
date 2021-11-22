3a2bf668b5c1507f86d55ed3a421e35078622ba1
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-11-22 17:28:40 +0800

    (100/100) for both slots. Still using Floyd-Warshall, i only use 1 array on this case tho

--- Lab/Lab7.java
+++ Lab/Lab7.java
@@ -61,7 +61,7 @@ public class Lab7 {
         }
 
         // Fill the graph
-        graph = floydWarshall(graph, N);
+        floydWarshall(graph, N);
         while (Q-- > 0) {
             int X = in.nextInt();
             int Y = in.nextInt();
@@ -72,20 +72,21 @@ public class Lab7 {
     }
 
     // Template from https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16
-    public static int[][] floydWarshall(int graph[][], int N) {
-        int dist[][] = new int[N][N];
+    public static void floydWarshall(int graph[][], int N) {
         int i, j, k;
 
-        /*
-         * Initialize the solution matrix same as input graph matrix. Or we can say the
-         * initial values of shortest distances are based on shortest paths considering
-         * no intermediate vertex.
-         */
-        for (i = 0; i < N; i++)
-            for (j = i; j < N; j++) {
-                dist[i][j] = graph[i][j];
-                dist[j][i] = graph[i][j];
-            }
+        // /*
+        // * Initialize the solution matrix same as input graph matrix. Or we can say
+        // the
+        // * initial values of shortest distances are based on shortest paths
+        // considering
+        // * no intermediate vertex.
+        // */
+        // for (i = 0; i < N; i++)
+        // for (j = i; j < N; j++) {
+        // dist[i][j] = graph[i][j];
+        // dist[j][i] = graph[i][j];
+        // }
 
         /*
          * Add all vertices one by one to the set of intermediate vertices. ---> Before
@@ -103,18 +104,18 @@ public class Lab7 {
                 for (j = i; j < N; j++) {
                     // If vertex k is on the shortest path from
                     // i to j, then update the value of dist[i][j]
-                    if (dist[i][k] != INF && dist[k][j] != INF) {
-                        if (dist[i][k] + dist[k][j] < dist[i][j]) {
-                            dist[i][j] = dist[i][k] + dist[k][j];
+                    if (graph[i][k] != INF && graph[k][j] != INF) {
+                        if (graph[i][k] + graph[k][j] < graph[i][j]) {
+                            graph[i][j] = graph[i][k] + graph[k][j];
 
                             // Karena directed graph.
-                            dist[j][i] = dist[i][k] + dist[k][j];
+                            graph[j][i] = graph[i][k] + graph[k][j];
                         }
                     }
                 }
             }
         }
-        return dist;
+        // return dist;
     }
 
     public static void printSolution(int dist[][], int N) {
