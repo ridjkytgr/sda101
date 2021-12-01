7c03c62a4da34b57c1dc0a7c0b13e837d08cde3a
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-12-02 07:33:46 +0800

    Implement carry with OOP Karyawan

--- TP/TP3.java
+++ TP/TP3.java
@@ -9,8 +9,7 @@ import java.util.*;
 public class TP3 {
     private static InputReader in;
     public static PrintWriter out;
-    public static int[] pangkat = new int[100000];
-    public static MaxHeap[] adjList = new MaxHeap[100000];
+    public static Karyawan[] dataKaryawan = new Karyawan[100000];
 
     /**
      * Method untuk menambahkan hubungan antara karyawan ke-u dan karyawan ke-v.
@@ -18,9 +17,9 @@ public class TP3 {
      * @param u karyawan ke-u.
      * @param v karyawan ke-v.
      */
-    public static void tambah(int u, int v) {
-        adjList[u - 1].insert(v - 1);
-        adjList[v - 1].insert(u - 1);
+    public static void tambah(Graph graf, int u, int v) {
+        graf.getAdj(u).insert(dataKaryawan[u - 1], dataKaryawan[v - 1]);
+        graf.getAdj(v).insert(dataKaryawan[v - 1], dataKaryawan[u - 1]);
     }
 
     // public static void resign(int u) {
@@ -43,11 +42,11 @@ public class TP3 {
      * @param u karyawan yang akan dicari teman tertingginya.
      * @return pangkat tertinggi dari teman u.
      */
-    public static int carry(int u) {
-        if (adjList[u - 1].getMax() < 1) { // Menandakan kalau u belum punya teman.
+    public static int carry(Graph graf, int u) {
+        if (graf.getAdj(u).getMax().getIdentitas() < 1) { // Menandakan kalau u belum punya teman.
             return 0;
         }
-        return pangkat[adjList[u - 1].getMax()];
+        return graf.getAdj(u).getMax().getPangkat();
     }
 
     public static void main(String[] args) {
@@ -69,12 +68,13 @@ public class TP3 {
         // Banyak kejadian.
         int q = in.nextInt();
 
+        Graph graf = new Graph(n);
+
         // Initialize pangkat-pangkat karyawan
         for (int i = 0; i < n; i++) {
             int pangkatKaryawan = in.nextInt();
 
-            pangkat[i] = pangkatKaryawan;
-            adjList[i] = new MaxHeap(100000);
+            dataKaryawan[i] = new Karyawan(i + 1, pangkatKaryawan);
         }
 
         // Hubungan antarkaryawwan
@@ -82,9 +82,13 @@ public class TP3 {
             int m1 = in.nextInt();
             int m2 = in.nextInt();
 
+            MaxHeap adjList1 = graf.getAdj(m1);
+            MaxHeap adjList2 = graf.getAdj(m2);
+
             // Karena undirected graph.
-            adjList[m1 - 1].insert(m2 - 1);
-            adjList[m2 - 1].insert(m1 - 1);
+            adjList1.insert(dataKaryawan[m1 - 1], dataKaryawan[m2 - 1]);
+            adjList2.insert(dataKaryawan[m2 - 1], dataKaryawan[m1 - 1]);
+
         }
 
         // Kejadian
@@ -95,7 +99,7 @@ public class TP3 {
                 int u = in.nextInt();
                 int v = in.nextInt();
 
-                tambah(u, v);
+                tambah(graf, u, v);
             } else if (kode == 2) { // Resign
                 int u = in.nextInt();
 
@@ -103,7 +107,7 @@ public class TP3 {
             } else if (kode == 3) { // Carry
                 int u = in.nextInt();
 
-                out.println(carry(u));
+                out.println(carry(graf, u));
             } else if (kode == 4) {
 
             } else if (kode == 5) {
@@ -154,7 +158,7 @@ public class TP3 {
 
     static class MaxHeap {
         // Member variables of this class
-        private int[] Heap;
+        private Karyawan[] Heap;
         private int size;
         private int maxsize;
 
@@ -168,8 +172,8 @@ public class TP3 {
             this.maxsize = maxsize;
             this.size = 0;
 
-            Heap = new int[this.maxsize + 1];
-            Heap[0] = Integer.MIN_VALUE;
+            Heap = new Karyawan[this.maxsize + 1];
+            Heap[0] = new Karyawan(0, Integer.MIN_VALUE);
         }
 
         // Method 1
@@ -209,68 +213,76 @@ public class TP3 {
 
         // Method 5
         // To swap two nodes of the heap
-        private void swap(int fpos, int spos) {
+        private void swap(Karyawan reference, int fpos, int spos) {
 
-            int tmp;
+            Karyawan tmp;
             tmp = Heap[fpos];
+
+            Heap[fpos].setLetakHeapWithWho(reference.getIdentitas(), spos);
+            Heap[spos].setLetakHeapWithWho(reference.getIdentitas(), fpos);
 
             Heap[fpos] = Heap[spos];
             Heap[spos] = tmp;
         }
 
-        // Method 6
-        // To heapify the node at pos
-        private void maxHeapify(int pos) {
-            // If the node is a non-leaf node and greater
-            // than any of its child
-            if (!isLeaf(pos)) {
-                if (pangkat[pos] < pangkat[leftChild(pos)]
-                        || pangkat[pos] < pangkat[rightChild(pos)]) {
+        // // Method 6
+        // // To heapify the node at pos
+        // private void maxHeapify(Karyawan reference, int pos) {
+        // // If the node is a non-leaf node and greater
+        // // than any of its child
+        // if (!isLeaf(pos)) {
+        // if (Heap[pos] < Heap[leftChild(pos)])
+        // || pangkat[pos] < pangkat[rightChild(pos)]) {
 
-                    // Swap with the left child and heapify
-                    // the left child
-                    if (pangkat[leftChild(pos)] > pangkat[rightChild(pos)]) {
-                        swap(pos, leftChild(pos));
-                        maxHeapify(leftChild(pos));
-                    }
+        // // Swap with the left child and heapify
+        // // the left child
+        // if (pangkat[leftChild(pos)] > pangkat[rightChild(pos)]) {
+        // swap(reference, pos, leftChild(pos));
+        // maxHeapify(reference, leftChild(pos));
+        // }
 
-                    // Swap with the right child and heapify
-                    // the right child
-                    else {
-                        swap(pos, rightChild(pos));
-                        maxHeapify(rightChild(pos));
-                    }
-                }
-            }
-        }
+        // // Swap with the right child and heapify
+        // // the right child
+        // else {
+        // swap(reference, pos, rightChild(pos));
+        // maxHeapify(reference, rightChild(pos));
+        // }
+        // }
+        // }
+        // }
 
         // Method 7
         // To insert a node into the heap
-        public void insert(int element) {
+        public void insert(Karyawan reference, Karyawan element) {
 
             if (size >= maxsize) {
                 return;
             }
 
             Heap[++size] = element;
+            element.setLetakHeapWithWho(reference.getIdentitas(), size);
             int current = size;
 
-            while (pangkat[current] < pangkat[parent(current)]) {
-                swap(current, parent(current));
+            while (Heap[current].getPangkat() < Heap[parent(current)].getPangkat()) {
+                swap(reference, current, parent(current));
                 current = parent(current);
             }
         }
 
         // Method 8
         // To print the contents of the heap
-        public StringBuilder print() {
+        public StringBuilder print(Karyawan reference) {
             StringBuilder sb = new StringBuilder();
             for (int i = 1; i <= size / 2; i++) {
 
                 // Printing the parent and both childrens
-                sb.append(" PARENT : " + pangkat[Heap[i]] + "(" + Heap[i] + ")" + " LEFT CHILD : "
-                        + pangkat[Heap[2 * i]] + "(" + Heap[2 * i] + ")" + " RIGHT CHILD :"
-                        + pangkat[Heap[2 * i + 1]] + "(" + Heap[2 * i + 1] + ")");
+                sb.append(" PARENT : " + Heap[i].getPangkat() + "("
+                        + Heap[i].getLetakHeapWithWho(reference.getIdentitas()) + ")"
+                        + " LEFT CHILD : "
+                        + Heap[2 * i].getPangkat() + "(" + Heap[2 * i].getLetakHeapWithWho(reference.getIdentitas())
+                        + ")" + " RIGHT CHILD :"
+                        + Heap[2 * i + 1].getPangkat() + "("
+                        + Heap[2 * i + 1].getLetakHeapWithWho(reference.getIdentitas()) + ")");
 
                 // By here new line is required
                 sb.append("\n");
@@ -278,22 +290,62 @@ public class TP3 {
             return sb;
         }
 
-        // Method 9
-        // To remove and return the minimum
-        // element from the heap
-        public int remove() {
-
-            int popped = Heap[FRONT];
-            Heap[FRONT] = Heap[size--];
-            maxHeapify(FRONT);
-
-            return popped;
-        }
-
         // Method 10
         // Getter for the minimal element
-        public int getMax() {
+        public Karyawan getMax() {
             return Heap[FRONT];
+        }
+    }
+
+    static class Karyawan {
+        private int identitas;
+        private int pangkat;
+        private int[] letakHeap;
+
+        public Karyawan(int identitas, int pangkat) {
+            this.identitas = identitas;
+            this.pangkat = pangkat;
+
+            // Untuk menyimpan letak karyawan pada adj orang lain.
+            this.letakHeap = new int[100000];
+        }
+
+        public int getIdentitas() {
+            return this.identitas;
+        }
+
+        public int getPangkat() {
+            return this.pangkat;
+        }
+
+        public int getLetakHeapWithWho(int karyawanTetangga) {
+            return letakHeap[karyawanTetangga - 1];
+        }
+
+        public void setLetakHeapWithWho(int karyawanTetangga, int letakBaru) {
+            letakHeap[karyawanTetangga - 1] = letakBaru;
+        }
+    }
+
+    static class Graph {
+        private int numOfVertex;
+        private MaxHeap adj;
+        private MaxHeap[] arrOfAdj;
+
+        public Graph(int numOfVertex) {
+            this.numOfVertex = numOfVertex;
+
+            arrOfAdj = new MaxHeap[numOfVertex];
+
+            // Membuat arrayOfMaxHeaps untuk menyatakan adj list. Index 0 Karyawan ke-1
+            for (int i = 0; i < numOfVertex; i++) {
+                adj = new MaxHeap(100000);
+                arrOfAdj[i] = adj;
+            }
+        }
+
+        public MaxHeap getAdj(int karyawanKe) {
+            return arrOfAdj[karyawanKe - 1];
         }
     }
 }
